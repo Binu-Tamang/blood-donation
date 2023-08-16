96a5e7ac4e110772863a7c5cbb631c7d75c44a43
unknown <binu@mapleleapgroups.com>
2023-08-16 16:46:10 +0545

    blood donation home page

---
+++ inc/css.php
@@ -0,0 +1,26 @@
+<!-- google fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700;800;900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+<!-- bodoni fonts styles -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+
+<!-- Fontawesome cdn -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+<!-- bootstrap.js -->
+<link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<!-- magnific pop up csss -->
+<link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+<!-- Aos css -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<!-- owl css -->
+<link rel="stylesheet" href="assets/plugins/owl/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/plugins/owl/css/owl.theme.default.min.css">
+<!-- style css -->
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
---
+++ inc/header.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="header-one">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="header-one-left">
+                        <a href="#"><i class="fa-solid fa-droplet"></i> <span>BLOOD DOST</span></a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="header-one-right">
+                        <ul>
+                            <li><a href="#" class="login"><i class="fa-solid fa-user"></i> Login/Register</a></li>
+                            <li><a href="#">Terms & Condition</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-two">
+        <div class="container">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Why Donate blood</a></li>
+                <li><a href="#">Donor Details</a></li>
+                <li><a href="#">Blood Banks</a></li>
+                <li><a href="#">Feedback</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
